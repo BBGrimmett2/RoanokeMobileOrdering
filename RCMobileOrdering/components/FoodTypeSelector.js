c92933b34285f7025699a6eb6c1e86f2351a5f77
lhelder <88600153+lhelder@users.noreply.github.com>
2022-04-06 10:38:46 -0400

    add foodtypeselector file

---
+++ RCMobileOrdering/components/FoodTypeSelector.js
@@ -0,0 +1,4 @@
+import { useNavigation } from "@react-navigation/core";
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { auth } from "../firebase";
