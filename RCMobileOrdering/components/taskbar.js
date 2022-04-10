303ae8f79847dfd0100c418a7243e8851ac0fb39
DAViS-TiNGLE <jdtingle@mail.roanoke.edu>
2022-04-10 16:53:51 -0400

    Tab Bar with Icons

--- RCMobileOrdering/components/taskbar.js
+++ RCMobileOrdering/components/taskbar.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/core";
 import React from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View, Image } from "react-native";
 import { auth } from "../firebase";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -19,20 +19,106 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
   //            correct screens
   const MyTabs = () => {
     return (
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={LoginScreen} />
-        <Tab.Screen name="Account" component={AccountScreen} />
+      <Tab.Navigator
+        tabBarOptions={{
+            showLabel: false,
+            style: {
+                position: 'absolute',
+                bottom: 25,
+                left: 20,
+                right: 20,
+                elevation: 0,
+                backgroundColor: '#ffffff',
+                borderRadius: 15,
+                height: 90,
+                ...styles.shadow
+            }
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} options={{
+            tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 0}}>
+                    <Image
+                        source={require('../pictures/home.png')}
+                        resizeMode='contain'
+                        style={{
+                            width: 25,
+                            height: 25,
+                            // tintColor: focused ? '#e32f45' : '#748c94'                            
+                        }}
+                    />
+                <Text 
+                    style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12 }}>
+                    Home
+                </Text>
+                </View>
+            ),
+            }}/>
+        <Tab.Screen name="Settings" component={LoginScreen} options={{
+            tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 0}}>
+                    <Image
+                        source={require('../pictures/settings_icon.png')}
+                        resizeMode='contain'
+                        style={{
+                            width: 25,
+                            height: 25,
+                            // tintColor: focused ? '#e32f45' : '#748c94'                            
+                        }}
+                    />
+                <Text 
+                    style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12 }}>
+                    Settings
+                </Text>
+                </View>
+            ),
+            }}/>
+        <Tab.Screen name="Account" component={AccountScreen} options={{
+            tabBarIcon: ({focused}) => (
+                <View style={{alignItems: 'center', justifyContent: 'center', top: 0}}>
+                    <Image
+                        source={require('../pictures/account_icon.png')}
+                        resizeMode='contain'
+                        style={{
+                            width: 25,
+                            height: 25,
+                            // tintColor: focused ? '#e32f45' : '#748c94'                            
+                        }}
+                    />
+                <Text 
+                    style={{color: focused ? '#e32f45' : '#748c94', fontSize: 12 }}>
+                    Account
+                </Text>
+                </View>
+            ),
+            }}/>
       </Tab.Navigator>
     );
   }
   
-  export default MyTabs;
 
-//   /* make account order and cart
+
+
+  
+
+
  
 
 const styles = StyleSheet.create({
+
+
+
+    shadow: {
+        shadowColor: '#7F5DF0',
+        shadowOffset: {
+            width: 0 ,
+            height: 10,
+
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.5,
+        elevation: 5,
+    },
 
     taskbar: {
         flex: 1,
@@ -67,3 +153,6 @@ const styles = StyleSheet.create({
         fontSize: 16,
     },
 });
+
+//   /* make account order and cart
+export default MyTabs;
