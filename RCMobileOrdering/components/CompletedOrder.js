7b2e118bf44397e9f6b9b2db2883d09bd9dc3d4d
BBGrimmett2 <66840892+BBGrimmett2@users.noreply.github.com>
2022-04-18 10:18:10 -0400

    Checkout screen

--- RCMobileOrdering/components/CompletedOrder.js
+++ RCMobileOrdering/components/CompletedOrder.js
@@ -1,23 +1,18 @@
 import React from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Image, TouchableHighlight } from "react-native";
 import { auth, fireDB, user } from "../firebase";
-
-
 import { NavigationContainer } from '@react-navigation/native';
 import { useNavigation } from "@react-navigation/core";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-
 function CompletedOrderScreen({ navigation }) {
-
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text> your order has been submitted</Text>
         <TouchableHighlight
          onPress={() => {
-         navigation.navigate('TaskBar', {
+         navigation.navigate('TypeSelection', {
          })
             }}>
 
--- RCMobileOrdering/components/ItemScreen.js
+++ RCMobileOrdering/components/ItemScreen.js
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/core";
 import { NavigationContainer } from "@react-navigation/native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import {
     FlatList,
@@ -14,7 +14,7 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { NativeScreenContainer } from "react-native-screens";
-import { auth, fireDB, userID } from "../firebase";
+import { auth, fireDB, userID, userCart } from "../firebase";
 
 const Customization = (props) => {
     //will need to lift state of customSelected to keep selections
@@ -70,6 +70,7 @@ const Item = ({ route }) => {
     const [nutrFacts, handleNFshow] = useState(false);
     const [customize, handleCustomizeshow] = useState(false);
     const navigation = useNavigation();
+    const [userCart, setuserCart] = useState([]);
 
     function showNF() {
         handleNFshow((nutrFacts) => !nutrFacts);
@@ -78,23 +79,34 @@ const Item = ({ route }) => {
         handleCustomizeshow((customize) => !customize);
     }
 
-    const handleAddToCart = async () => {
+    const getData = async () => {
+        const userRef = fireDB.collection("users").doc(userID);
+        const doc = await userRef.get();
+
+        return doc.data();
+    };
+
+    const addItemToCart = async () => {
         const userRef = fireDB.collection("users").doc(userID);
 
         // Set the 'userID' field of the cart
-        const res = await userRef.update({ cart: itemObj.name });
+        const res = await userRef.update({ cart: userCart });
+    };
+
+
+    const handleAddToCart = async () => {
+        addItemToCart();
 
         navigation.navigate("TaskBar");
     };
 
     const handleToCheckout = async () => {
-        const userRef = fireDB.collection("users").doc(userID);
-
-        // Set the 'userID' field of the cart
-        const res = await userRef.update({ cart: itemObj.name });
+        addItemToCart();
 
         navigation.navigate("Cart");
     };
+
+    console.log("user cart: " + userCart);
 
     return (
         <SafeAreaView style={styles.container}>
