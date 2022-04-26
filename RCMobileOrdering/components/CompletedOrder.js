9935f97f1e340b70af3a34eeda3a55ab1ed3aa40
Brian Grimmett <bbgrimmett2@gmail.com>
2022-04-25 21:59:53 -0400

    Edits

--- RCMobileOrdering/components/CompletedOrder.js
+++ RCMobileOrdering/components/CompletedOrder.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import {
     StyleSheet,
     Text,
@@ -8,9 +9,39 @@ import {
     TouchableHighlight,
 } from "react-native";
 import { useNavigation } from "@react-navigation/core";
+import { fireDB, userID } from "../firebase";
 
 function CompletedOrderScreen() {
     const navigation = useNavigation();
+
+    let emptyCart = [];
+    let [cart, setCart] = useState();
+    let [userReceipts, setReceipts] = useState([]);
+
+    const getData = async () => {
+        const userRef = fireDB.collection("users").doc(userID);
+        const doc = await userRef.get();
+
+        return doc.data();
+    };
+
+    getData().then((data) => {
+        setCart(data.cart);
+        setReceipts(data.receipts);
+    });
+    
+    const clearCart = async () => {
+        const userRef = fireDB.collection("users").doc(userID);
+
+        // Clear cart and set add to receipt array
+        const res = await userRef.update({ cart: emptyCart });
+    };
+
+    const handleCompletedOrder = () => {
+        clearCart();
+
+        navigation.navigate("HomeScreen");
+    };
 
     return (
         <View style={styles.screen}>
@@ -18,9 +49,7 @@ function CompletedOrderScreen() {
                 <Text style={{fontSize:20, fontWeight:"700", color: "#FFFFFF"}}> Your order has been submitted</Text>
             </View>
             <TouchableHighlight
-                onPress={() => {
-                    navigation.navigate("HomeScreen");
-                }}
+                onPress={handleCompletedOrder}
             >
                 <Text> Go Back Home </Text>
             </TouchableHighlight>
